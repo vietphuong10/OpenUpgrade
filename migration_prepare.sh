4e8fc60a394e2ab2a247a89dde6e1860b7ff041d
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-07-05 18:55:41 +0200

    [bump]

---
+++ migration_prepare.sh
@@ -0,0 +1,5 @@
+mv parts/odoo parts/OpenUpgrade
+mv parts/odoo_bk parts/odoo
+ak build
+mv parts/odoo parts/odoo_bk
+mv parts/OpenUpgrade parts/odoo
---
+++ migration_run.sh
@@ -0,0 +1,1 @@
+ak run -d resto_2017_06_27_a -u all --stop-after-init --log-level debug
