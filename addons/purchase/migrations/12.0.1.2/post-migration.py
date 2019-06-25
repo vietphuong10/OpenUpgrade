8421d4916144fa6fb617f80c2d5be0feb8b6bb63
David Tran <david.tran@tvtmarine.com>
2019-06-25 18:40:00 +0700

    [FIX] purchase,purchase_stock: updated from the under-reviewing PR

--- addons/purchase/migrations/12.0.1.2/post-migration.py
+++ addons/purchase/migrations/12.0.1.2/post-migration.py
@@ -13,9 +13,22 @@ def fill_purchase_order_user_id(cr):
     )
 
 
+def reset_domain_purchase_rfq(cr):
+    cr.execute(
+        """
+        UPDATE ir_act_window iaw
+        SET domain = NULL
+        FROM ir_model_data imd
+        WHERE imd.name = 'purchase_rfq' AND imd.res_id = iaw.id
+            AND imd.module = 'purchase'
+        """
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     fill_purchase_order_user_id(env.cr)
+    reset_domain_purchase_rfq(env.cr)
     openupgrade.load_data(
         env.cr, 'purchase', 'migrations/12.0.1.2/noupdate_changes.xml')
     openupgrade.delete_records_safely_by_xml_id(
