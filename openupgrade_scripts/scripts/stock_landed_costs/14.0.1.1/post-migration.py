9422c41cc31f756a7a8ff15cb89fa3c7f84f85c5
hoangtiendung <hoangtiendung070797@gmail.com>
2022-01-20 16:50:09 +0700

    [FIX]stock_landed_costs: path error

--- openupgrade_scripts/scripts/stock_landed_costs/14.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/stock_landed_costs/14.0.1.1/post-migration.py
@@ -6,4 +6,4 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     # Load noupdate changes
-    openupgrade.load_data(env.cr, "stock_landed_costs", "14.0.1.0/noupdate_changes.xml")
+    openupgrade.load_data(env.cr, "stock_landed_costs", "14.0.1.1/noupdate_changes.xml")
