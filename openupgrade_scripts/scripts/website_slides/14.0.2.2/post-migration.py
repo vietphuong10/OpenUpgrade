dc3b6ef060228e61a59d00318503f78f51507ed1
tranngocson1996 <transon821996@gmail.com>
2022-01-21 10:44:38 +0700

    [FIX] website_slide: fix delete translate record

--- openupgrade_scripts/scripts/website_slides/14.0.2.2/post-migration.py
+++ openupgrade_scripts/scripts/website_slides/14.0.2.2/post-migration.py
@@ -4,3 +4,13 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "website_slides", "14.0.2.2/noupdate_changes.xml")
+
+    openupgrade.delete_record_translations(
+        env.cr,
+        "website_slides",
+        [
+            "mail_template_slide_channel_invite",
+            "slide_template_published",
+            "slide_template_shared",
+        ],
+    )
