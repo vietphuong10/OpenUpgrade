e4e64b99e3fd3948f9cc9c13be080a9cf35d31f6
hoangdinhhieu <hieu.hoang121199@gmail.com>
2022-03-14 15:52:52 +0700

    Revert "[IMP]l10n_vn: correct account type" and apply new fix
    This reverts commit 03329ceff27cc37371433beb29c603e796adb115.

---
+++ openupgrade_scripts/scripts/account/14.0.1.1/end-migration.py
@@ -0,0 +1,30 @@
+from openupgradelib import openupgrade
+
+
+def _make_correct_account_type(env):
+    query = """ UPDATE account_account as ac
+        SET user_type_id=aat.user_type_id
+        FROM account_account_template as aat
+        LEFT JOIN account_chart_template as act
+                    ON aat.chart_template_id = act.id
+        LEFT JOIN res_company as c
+                    ON c.chart_template_id = act.id
+        WHERE ac.code =
+                CASE
+                    WHEN
+                        act.code_digits < LENGTH(aat.code) THEN aat.code
+                    ELSE
+                        CONCAT(aat.code,
+                            REPEAT('0',act.code_digits - LENGTH(aat.code)))
+                END
+            AND ac.user_type_id != aat.user_type_id
+            AND ac.company_id = c.id"""
+    openupgrade.logged_query(
+        env.cr,
+        query,
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _make_correct_account_type(env)
