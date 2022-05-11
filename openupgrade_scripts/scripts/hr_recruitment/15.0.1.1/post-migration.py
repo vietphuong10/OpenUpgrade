666c55b403f73a4bd85206d0ac27d64e087f61b0
sang250399 <sangfpt1999alhp@gmail.com>
2022-05-11 13:35:39 +0700

    [MIG]hr_recruitment_survey: Nothing to do

--- openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/post-migration.py
@@ -3,7 +3,13 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    # Load noupdate changes
-    openupgrade.load_data(
-        env.cr, "hr_recruitment", "15.0.1.1/noupdate_changes.xml"
+    openupgrade.load_data(env.cr, "hr_recruitment", "15.0.1.1/noupdate_changes.xml")
+    openupgrade.delete_record_translations(
+        env.cr,
+        "hr_recruitment",
+        [
+            "email_template_data_applicant_congratulations",
+            "email_template_data_applicant_interest",
+            "email_template_data_applicant_refuse",
+        ],
     )
--- openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/pre-migration.py
@@ -6,3 +6,6 @@ def migrate(env, version):
     openupgrade.set_xml_ids_noupdate_value(
         env, "hr_recruitment", ["applicant_hired_template"], True
     )
+    openupgrade.convert_field_to_html(
+        env.cr, "hr_applicant", "description", "description"
+    )
