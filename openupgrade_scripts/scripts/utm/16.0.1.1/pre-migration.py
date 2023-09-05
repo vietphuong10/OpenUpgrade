bdf802e7bbc78eb3db7266f256ad395e77a30648
Roy Le <roy.le@viindoo.com>
2023-09-05 09:30:50 +0700

    [FIX] utm: fix psycopg2.errors.DatatypeMismatch
    psycopg2.errors.DatatypeMismatch: column "title" is of type jsonb but
    expression is of type character varying
    UPDATE utm_campaign SET title=name;

--- openupgrade_scripts/scripts/utm/16.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/utm/16.0.1.1/pre-migration.py
@@ -3,7 +3,42 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-
     # pre-migration of module base converted name column from utm_campaign
     # into jsonb, hence it suffices to copy the name column into title.
-    openupgrade.copy_columns(env.cr, {"utm_campaign": [("name", "title", "jsonb")]})
+    if not openupgrade.column_exists(env.cr, "utm_campaign", "title"):
+        openupgrade.logged_query(
+            env.cr,
+            """
+            ALTER TABLE utm_campaign
+            ADD COLUMN IF NOT EXISTS title jsonb;
+            """,
+        )
+        query = """
+            WITH t AS (
+                SELECT it.res_id as res_id,
+                    jsonb_object_agg(it.lang, it.value) AS value,
+                    bool_or(imd.noupdate) AS noupdate
+                  FROM _ir_translation it
+             LEFT JOIN ir_model_data imd
+                    ON imd.model = 'utm_campaign' AND imd.res_id = it.res_id
+                 WHERE it.type = 'model' AND it.name = 'utm.campaign,name'
+                     AND it.state = 'translated'
+              GROUP BY it.res_id
+            )
+            UPDATE utm_campaign m
+               SET title = CASE
+                   WHEN t.noupdate
+                   THEN t.value || jsonb_build_object('en_US', m.name::varchar)
+                   ELSE t.value || jsonb_build_object('en_US', m.name::varchar)
+                END
+              FROM t
+             WHERE t.res_id = m.id
+        """
+        openupgrade.logged_query(env.cr, query)
+        openupgrade.logged_query(
+            env.cr,
+            """UPDATE utm_campaign
+               SET title = jsonb_build_object('en_US', name::varchar)
+             WHERE title IS NULL
+            """,
+        )
