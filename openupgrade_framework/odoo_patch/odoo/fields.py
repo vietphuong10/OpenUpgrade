eb1260017f4c80734ff285cc0d677cdf0ae4534b
Roy Le <roy.le@viindoo.com>
2023-09-09 09:36:09 +0700

    [FIX] odoo_patch: avoid error when running upgrade_analysis

--- openupgrade_framework/odoo_patch/odoo/fields.py
+++ openupgrade_framework/odoo_patch/odoo/fields.py
@@ -3,6 +3,7 @@ import itertools
 from openupgradelib import openupgrade
 
 from odoo.fields import _String
+from odoo.tools.sql import table_exists
 from odoo.tools.translate import _get_translation_upgrade_queries
 
 
@@ -10,7 +11,9 @@ def _convert_db_column(self, model, column):
     _String._convert_db_column._original_method(self, model, column)
     # update all translatable fields
     # specialized implementation for converting from/to translated fields
-    if self.translate or column["udt_name"] == "jsonb":
+    if (self.translate or column["udt_name"] == "jsonb") and table_exists(
+        model._cr, "_ir_translation"
+    ):
         for query in itertools.chain.from_iterable(
             _get_translation_upgrade_queries(model._cr, self)
         ):
