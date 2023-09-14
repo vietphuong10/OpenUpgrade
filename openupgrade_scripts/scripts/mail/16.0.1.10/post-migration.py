dbaea7807fde253d942203084ff78afeacd6efb2
Roy Le <roy.le@viindoo.com>
2023-09-14 14:47:00 +0700

    [FIX] mail: do not show new messages separator for own messages

--- openupgrade_scripts/scripts/mail/16.0.1.10/post-migration.py
+++ openupgrade_scripts/scripts/mail/16.0.1.10/post-migration.py
@@ -4,6 +4,19 @@
 from openupgradelib import openupgrade
 
 
+def _set_last_seen_message(env):
+    # do not show new messages separator for own messages
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_channel_member
+            SET seen_message_id = fetched_message_id
+        WHERE seen_message_id IS NULL OR seen_message_id < fetched_message_id
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "mail", "16.0.1.10/noupdate_changes.xml")
+    _set_last_seen_message(env)
