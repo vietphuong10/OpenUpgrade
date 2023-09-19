bce978b7ae600e70ba86e4e6ed3e125437710162
Giang Pham <phamgiang2510@gmail.com>
2023-09-19 17:05:56 +0700

    [FIX] sale_quotation_builder: Convert bootstrap 5 for quotation templates

--- openupgrade_scripts/scripts/sale_quotation_builder/16.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/sale_quotation_builder/16.0.1.0/post-migration.py
@@ -1,8 +1,15 @@
 from openupgradelib import openupgrade
+from openupgradelib.openupgrade_160 import convert_string_bootstrap_4to5
+
+
+def _bs5_field_migration(env):
+    templates = env["sale.order.template"].with_context(active_test=False).search([])
+    for template in templates:
+        new_description = convert_string_bootstrap_4to5(template.website_description)
+        if template.website_description != new_description:
+            template.website_description = new_description
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, "sale_quotation_builder", "16.0.1.0/noupdate_changes.xml"
-    )
+    _bs5_field_migration(env)
