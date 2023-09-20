a5e3548853c85fa05612ecd4945adb4291cbe268
nguyenvietlam0640 <lamnv0640@gmail.com>
2023-09-20 10:42:59 +0700

    [IMP] project: active milestone group in post - migration

--- openupgrade_scripts/scripts/project/16.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/project/16.0.1.2/post-migration.py
@@ -1,6 +1,11 @@
 from openupgradelib import openupgrade
 
 
+def active_group_project_milestone(env):
+    env["res.config.settings"].create({"group_project_milestone": True}).execute()
+
+
 @openupgrade.migrate()
 def migrate(env, version):
+    active_group_project_milestone(env)
     openupgrade.load_data(env.cr, "project", "16.0.1.2/noupdate_changes.xml")
