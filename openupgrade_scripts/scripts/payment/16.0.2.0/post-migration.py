3c18005f949e10cc3f5244d438527c4d65abbcd0
Roy Le <roy.le@viindoo.com>
2023-10-06 14:43:03 +0700

    [FIX] payment: fill payment_provider'is_published

--- openupgrade_scripts/scripts/payment/16.0.2.0/post-migration.py
+++ openupgrade_scripts/scripts/payment/16.0.2.0/post-migration.py
@@ -4,6 +4,18 @@
 from openupgradelib import openupgrade
 
 
+def fill_payment_provider_is_published(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE payment_provider
+            SET is_published = True
+        WHERE state IN ('enabled', 'test')
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "payment", "16.0.2.0/noupdate_changes.xml")
+    fill_payment_provider_is_published(env)
